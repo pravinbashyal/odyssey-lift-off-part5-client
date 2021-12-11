278ad7a714476d880829d1133f185551cde7a8cc
Pravin Bashyal <pravin.bashyal@gmail.com>
2021-12-11 10:38:21 +0100

    log

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import GlobalStyles from './styles';
 import Pages from './pages';
 import {ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client';
 
+console.log('apollo url', process.env.APOLLO_URL)
+
 const client = new ApolloClient({
   uri: process.env.APOLLO_URL || 'https://localhost:4000',
   cache: new InMemoryCache(),
